c141824f090b3a222a9c695568e58875c3d950a7
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2025-10-22 12:42:41 +0200

    Advanced with what was seen in the last class and completed both functional job filtering and pagination.

--- 01-javascript/scripts.js
+++ 01-javascript/scripts.js
@@ -1,71 +1,31 @@
-// recupera solo el primer boton que encuentre
-// const boton = document.querySelector('.button-apply-job')
-// console.log(boton) // null si no lo encuentra
+const jobsListingSection = document.querySelector(".jobs-listings");
 
-// if (boton !== null) {
-//   boton.addEventListener('click', function() {
-//     boton.textContent = '¡Aplicado!'
-//     boton.classList.add('is-applied')
-//     boton.disabled = true
-//   })
-// }
+jobsListingSection.addEventListener("click", function (event) {
+  const element = event.target;
 
-// const botones = document.querySelectorAll('.button-apply-job')
-
-
-// botones.forEach(boton => {
-//   boton.addEventListener('click', function() {
-//     boton.textContent = '¡Aplicado!'
-//     boton.classList.add('is-applied')
-//     boton.disabled = true
-//   })
-// })
-
-const jobsListingSection = document.querySelector('.jobs-listings')
-
-jobsListingSection.addEventListener('click', function(event) {
-  const element = event.target
-
-  if (element.classList.contains('button-apply-job')) {
-    element.textContent = '¡Aplicado!'
-    element.classList.add('is-applied')
-    element.disabled = true
+  if (element.classList.contains("button-apply-job")) {
+    element.textContent = "¡Aplicado!";
+    element.classList.add("is-applied");
+    element.disabled = true;
   }
-})
+});
 
-const filter = document.querySelector('#filter-location')
-const mensaje = document.querySelector('#filter-selected-value')
-const jobs = document.querySelectorAll('.job-listing-card')
+const filter = document.querySelector("#filter-location");
+const mensaje = document.querySelector("#filter-selected-value");
+const jobs = document.querySelectorAll(".job-listing-card");
 
-filter.addEventListener('change', function () {
-  const selectedValue = filter.value
-  
+filter.addEventListener("change", function () {
+  const selectedValue = filter.value;
+
   if (selectedValue) {
-    mensaje.textContent = `Has seleccionado: ${selectedValue}`
+    mensaje.textContent = `Has seleccionado: ${selectedValue}`;
   } else {
-    mensaje.textContent = ''
+    mensaje.textContent = "";
   }
 
-  jobs.forEach(job => {
-    // const modalidad = job.dataset.modalidad
-    const modalidad = job.getAttribute('data-modalidad')
-    const isShown = selectedValue === '' || modalidad === selectedValue
-    job.style.display = isShown ? 'block' : 'none' 
-  })
-})
-
-
-
-// searchInput.addEventListener('input', function() {
-//   console.log(value)
-// })
-
-// searchInput.addEventListener('input', function() {
-//   console.log(searchInput.value)
-// })
-
-// const searchForm = document.querySelector('#empleos-search-form') 
-// searchForm.addEventListener('submit', function() {
-//   event.preventDefault()
-//   console.log('submit')
-// })
+  jobs.forEach((job) => {
+    const modalidad = job.getAttribute("data-modalidad");
+    const isShown = selectedValue === "" || modalidad === selectedValue;
+    job.style.display = isShown ? "block" : "none";
+  });
+});
